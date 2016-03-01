3b52008445de6e229c924321776d301353b4b0a4
TERRENCE WHITTAKER <terr.whittakerjr@gmail.com>
2016-03-01 11:20:25 -0500

    set redirect to user profile after login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160229165640) do
+ActiveRecord::Schema.define(version: 20160301154142) do
 
   create_table "books", force: :cascade do |t|
     t.string   "book_title"
@@ -60,6 +60,8 @@ ActiveRecord::Schema.define(version: 20160229165640) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "favorite_book"
+    t.text     "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
